9f605e05fea09667f81f4013da7aee6aca1c7c48
Tawan123 <tawan.ch@waylar.net>
2025-07-27 05:19:25 +0700

    fix: deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  type: "postgres",
+  host: process.env.DB_HOST,
   port: 5432,
-  username: process.env.DB_USER || 'foody',
-  password: process.env.DB_PASS || 'foody123',
-  database: process.env.DB_NAME || 'foodydb',
-  synchronize: true, // สำหรับ dev เท่านั้น
-  logging: false,
-  entities: [__dirname + '/src/entity/**/*.js'],
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: process.env.TYPEORM_SYNC, // สำหรับ dev เท่านั้น
+  logging: process.env.TYPEORM_LOGGING,
+  entities: [__dirname + "/src/entity/**/*.js"],
 };
